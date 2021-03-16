cedecda351648b719ad74d3ac4339ae6ba088cf3
msk9308 <msk9308@naver.com>
2021-03-16 14:21:35 +0900

    처음

---
+++ Names.java
@@ -0,0 +1,11 @@
+// 여기서부터 자바 코드가 시작됨
+// comment, 주석 문법과 관계 없이
+// 자유롭게 아무 내용이나 입력 할 수 있는 영역
+// 설명문 등을 작성한다.
+public class Names{
+
+	public static void main(String[] arg){
+
+		System.out.println("I am 000");
+	}
+}
