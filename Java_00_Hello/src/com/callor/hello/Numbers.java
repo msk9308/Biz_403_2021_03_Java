a5ec6beca874317bea196a805ce87d4491fc5143
msk9308 <msk9308@naver.com>
2021-03-17 09:20:34 +0900

    복습

--- Java_00_Hello/src/com/callor/hello/Numbers.java
+++ Java_00_Hello/src/com/callor/hello/Numbers.java
@@ -1,12 +1,11 @@
 package com.callor.hello;
 
-public class Java_00_Hello {
-
+public class Numbers {
 	public static void main(String[] args) {
 		System.out.println("33 + 2 = " + ( 33 + 2 ));
 		System.out.println("33 - 2 = " + ( 33 - 2 ));
-		System.out.println("33 * 2 =" + (33 * 2));
-		System.out.println("33 / 2 =" + (33 / 2));
-		System.out.println("33 % 2 =" + (33 % 2));
+		System.out.println("33 * 2 =" + 33 * 2);
+		System.out.println("33 / 2 =" + 33 / 2);
+		System.out.println("33 % 2 =" + 33 % 2);
 	}
 }
