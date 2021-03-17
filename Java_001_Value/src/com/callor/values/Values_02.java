d25e1c8632338c9202db14bf670f8db1a42c46bd
msk9308 <msk9308@naver.com>
2021-03-17 11:50:32 +0900

    Values

--- Java_001_Value/src/com/callor/values/Values_02.java
+++ Java_001_Value/src/com/callor/values/Values_02.java
@@ -2,7 +2,7 @@ package com.callor.values;
 
 public class Values_02 {
 	public static void main(String[] args) {
-		System.out.println("원의둘레 :" + 23 * 2 * 3.14 );
+		System.out.println("원의 둘레 :" + 23 * 2 * 3.14 );
 		System.out.println("원의 넓이 :" + 23 * 23 * 3.14);
 	}
 }
