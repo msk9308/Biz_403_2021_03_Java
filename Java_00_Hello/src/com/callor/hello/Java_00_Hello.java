8e054cc9e24e647c87bbca409a0c41aa9f1519ef
msk9308 <msk9308@naver.com>
2021-03-17 09:15:03 +0900

    복습

---
+++ Java_00_Hello/src/com/callor/hello/Java_00_Hello.java
@@ -0,0 +1,12 @@
+package com.callor.hello;
+
+public class Java_00_Hello {
+
+	public static void main(String[] args) {
+		System.out.println("33 + 2 = " + ( 33 + 2 ));
+		System.out.println("33 - 2 = " + ( 33 - 2 ));
+		System.out.println("33 * 2 =" + (33 * 2));
+		System.out.println("33 / 2 =" + (33 / 2));
+		System.out.println("33 % 2 =" + (33 % 2));
+	}
+}
