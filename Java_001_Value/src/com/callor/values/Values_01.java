f227643156ade4dd687b299b3fd5458fecbe4391
msk9308 <msk9308@naver.com>
2021-03-17 10:25:48 +0900

    원

---
+++ Java_001_Value/src/com/callor/values/Values_01.java
@@ -0,0 +1,60 @@
+package com.callor.values;
+
+
+/*
+ * values_01 클래스의 정의
+ * 여기에서 부터 자바 코드가 시작된다
+ */
+
+public class Values_01 {
+
+	/*
+	 * main() 메소드(method)
+	 * java코드를 컴파일하고 실행을 할 때
+	 * 작동되는 코드의 시작
+	 * =시작점 메소드
+	 */
+	public static void main(String[] args) {
+		
+		/*
+		 * 컴퓨터를 사용하여 코딩을 하는 이유
+		 * (복잡한) 연산을 컴퓨터의 성능을  사용하여
+		 * 쉽게 수행하기 위함이다
+		 * 단순하게 전자계산기를 사용할 수 있지만
+		 * 코딩을 해두면 언제든지 필요할때
+		 * 다시 실행하여 (run)하여 결과를 확인가능
+		 */
+		
+		System.out.println("=================");
+		System.out.println("5단 구구단");
+		System.out.println("-----------------");
+		System.out.println(" 5 X 1 =" + (5 * 1));
+		System.out.println(" 5 X 2 =" + (5 * 1));
+		System.out.println(" 5 X 3 =" + (5 * 1));
+		System.out.println(" 5 X 4 =" + (5 * 1));
+		System.out.println(" 5 X 5 =" + (5 * 1));
+		System.out.println(" 5 X 6 =" + (5 * 1));
+		System.out.println(" 5 X 7 =" + (5 * 1));
+		System.out.println(" 5 X 8 =" + (5 * 1));
+		System.out.println(" 5 X 9 =" + (5 * 1));
+		System.out.println("=================");
+
+		System.out.println("=================");
+		System.out.println("7단 구구단");
+		System.out.println("-----------------");
+		System.out.println(" 7 X 1 =" + (5 * 1));
+		System.out.println(" 7 X 2 =" + (5 * 1));
+		System.out.println(" 7 X 3 =" + (5 * 1));
+		System.out.println(" 7 X 4 =" + (5 * 1));
+		System.out.println(" 7 X 5 =" + (5 * 1));
+		System.out.println(" 7 X 6 =" + (5 * 1));
+		System.out.println(" 7 X 7 =" + (5 * 1));
+		System.out.println(" 7 X 8 =" + (5 * 1));
+		System.out.println(" 7 X 9 =");
+		System.out.println("=================");
+
+		
+		
+	}
+
+}
---
+++ Java_001_Value/src/module-info.java
@@ -0,0 +1,2 @@
+module Java_001_Value {
+}
