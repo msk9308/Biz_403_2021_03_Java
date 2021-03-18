fd59101172bfd688d9d32790186062e4b9275f1e
msk9308 <msk9308@naver.com>
2021-03-18 16:39:34 +0900

    평균

---
+++ Java_002_Varriable/src/com/callor/var/Varriable_16.java
@@ -0,0 +1,60 @@
+package com.callor.var;
+
+public class Varriable_16 {
+	public static void main(String[] args) {
+		
+		int intDan = 3;
+		int intNum = 2;
+		int times = (intDan * intNum);
+		
+		
+		System.out.printf("%d x %d = %d\n", intDan , intNum, times  );
+		intNum += 1;
+		times = (intDan * intNum);
+		System.out.printf("%d x %d = %d\n", intDan , intNum, times  );
+		intNum += 1;
+		times = (intDan * intNum);
+		System.out.printf("%d x %d = %d\n", intDan , intNum, times  );
+		intNum += 1;
+		times = (intDan * intNum);
+		System.out.printf("%d x %d = %d\n", intDan , intNum, times  );
+		intNum += 1;
+		times = (intDan * intNum);
+		System.out.printf("%d x %d = %d\n", intDan , intNum, times  );
+		intNum += 1;
+		times = (intDan * intNum);
+		System.out.printf("%d x %d = %d\n", intDan , intNum, times  );
+		intNum += 1;
+		times = (intDan * intNum);
+		System.out.printf("%d x %d = %d\n", intDan , intNum, times  );
+		intNum += 1;
+		times = (intDan * intNum);
+		System.out.printf("%d x %d = %d\n", intDan , intNum, times  );
+		
+		
+		intDan = 3;
+		intNum = 2;
+		System.out.printf("%d x %d = %d\n", intDan , intNum, intDan * intNum);
+		intNum +=1;
+		System.out.printf("%d x %d = %d\n", intDan , intNum, intDan * intNum);
+		intNum +=1;
+		System.out.printf("%d x %d = %d\n", intDan , intNum, intDan * intNum);
+		intNum +=1;
+		System.out.printf("%d x %d = %d\n", intDan , intNum, intDan * intNum);
+		intNum +=1;
+		System.out.printf("%d x %d = %d\n", intDan , intNum, intDan * intNum);
+		intNum +=1;
+		System.out.printf("%d x %d = %d\n", intDan , intNum, intDan * intNum);
+		intNum +=1;
+		System.out.printf("%d x %d = %d\n", intDan , intNum, intDan * intNum);
+		intNum +=1;
+		System.out.printf("%d x %d = %d\n", intDan , intNum, intDan * intNum);
+		intNum +=1;
+		
+		
+		
+		
+				
+	}
+
+}
