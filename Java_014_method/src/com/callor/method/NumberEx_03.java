d6e9cf56ff6a5dc8db4fbc9f6093d79cd21866c1
msk9308 <msk9308@naver.com>
2021-04-07 11:24:20 +0900

    inputservice

---
+++ Java_014_method/src/com/callor/method/NumberEx_03.java
@@ -0,0 +1,12 @@
+package com.callor.method;
+
+import com.callor.service.ScoreServiceV3;
+
+public class NumberEx_03 {
+	
+	public static void main(String[] args) {
+		ScoreServiceV3 inService = new ScoreServiceV3();
+		inService.inputScore();
+	}
+
+}
--- Java_014_method/src/com/callor/service/ScoreServiceV1.java
+++ Java_014_method/src/com/callor/service/ScoreServiceV1.java
@@ -1,5 +1,15 @@
 package com.callor.service;
 
+
 public class ScoreServiceV1 {
+	
+	public Integer inputScore(String title, int start, int end) {
+		title = String.format("%s %d %d","국어",0,100);
+		Integer retNum = this.inputScore(title, start, end);
+ 		
+		
+		
+		
+	}
 
 }
