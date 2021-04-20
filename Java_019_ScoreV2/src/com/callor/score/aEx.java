eae9dbdd7ba383045b91407a9415cc4895d57e9b
msk9308 <msk9308@naver.com>
2021-04-20 16:37:57 +0900

    quiz

--- Java_019_ScoreV2/src/com/callor/score/aEx.java
+++ Java_019_ScoreV2/src/com/callor/score/aEx.java
@@ -4,9 +4,7 @@ public class aEx {
 	public static void main(String[] args) {
 		a sService = new a();
 		sService.card();
-		for(int i=0; i<3; i++) {
 		sService.randomAdd();
-		}
 		
 	}
 
--- Java_020_WordQuiz/src/com/callor/word/service/impl/WordServiceImplV1.java
+++ Java_020_WordQuiz/src/com/callor/word/service/impl/WordServiceImplV1.java
@@ -11,54 +11,58 @@ import java.util.Random;
 import java.util.Scanner;
 
 import com.callor.word.domain.WordVO;
+import com.callor.word.service.WordService;
 import com.msk9308.standard.InputService;
 import com.msk9308.standard.MenuService;
 import com.msk9308.standard.impl.InputServiceImplV1;
 
-public class WordServiceImplV1 {
+public class WordServiceImplV1 implements WordService {
 	
 	protected InputService inService;
-	protected MenuService mService;	
+	protected MenuService mService;
 	protected Scanner scan;
+	
+	protected List<WordVO> wordList; // word 들의 리스트
+	protected int nWordCount; // wordList의 개수를 담을 변수
+	
+	protected Random rnd;
 	
 	protected final int 영어 = 0;
 	protected final int 한글 = 1;
-	protected List<WordVO> wordList;//word들의 리스트
-	protected int nWordCount;//wordList 의 갯수
-	protected Random rnd;
 	
-	
-	
-	// word.txt파일을 기본으로 하여 
-	// 객체생성을 하기위한 생성자 
+	// word.txt 파일을 기본으로 하여 
+	//		객체생성을 하기위한 생성자
 	public WordServiceImplV1() {
 		this("src/com/callor/word/word.txt");
 	}
 	
-	//객체를 생성할때 파일이름을 전달하여
-	//word 파일을 지정하기
+	// 객체를 생성할때 파일이름을 전달하여
+	//	 word 파일을 지정하기
 	public WordServiceImplV1(String wordFile) {
-		// TODO Auto-generated constructor stub
 		inService = new InputServiceImplV1();
 		scan = new Scanner(System.in);
-		wordList = new ArrayList<WordVO>();
 		rnd = new Random();
+		
+		wordList = new ArrayList<WordVO>();
 		this.loadWords(wordFile);
-		//nWordCount는 wordList의 size를 담고 있을 변수인데
-		//가급적 값을 setting하는 코드를
-		//쉽게 발견 할 수 있도록 하기 위하여
-		//여기에 놓는다
+		
+		// nWordCount는 wordList의 size를 담고 있을 변수인데
+		// 가급적 값을 setting하는 코드를
+		// 쉽게 발견할수 있도록 하기 위하여
+		// 여기에 놓는다
 		nWordCount = wordList.size();
+		
+		
 	}
+
 	/*
 	 * 게임이 시작되면
-	 * 1.wordList에서 임의 단어 1개를 추출하고
-	 * 2.단어의 영문 스펠을 나누어 
-	 * 3.무작위로 섞고 
-	 * 4.섞인 단어를 보여주고 
-	 * 5.맞추기
+	 * 1. wordList에서 임의 단어 1개를 추출하고
+	 * 2. 단어의 영문 스펠을 나누어
+	 * 3. 무작위로 섞고
+	 * 4. 섞인 단어를 보여주고
+	 * 5. 맞추기
 	 */
-	
 	public void startGame() {
 		/*
 		 * getSuffleWord() method가 return하는
@@ -67,11 +71,11 @@ public class WordServiceImplV1 {
 		 * 
 		 * inputWord()를 호출하고
 		 * 다시 getSuffleWord() 호출하여
-		 * 수행할 수 있는 코드를
+		 * 수행할수 있는 코드를
 		 * 
 		 * getSuffleWord()를 호출하여
-		 * 데이터만(섞인 영단어) 되돌려받고
-		 * 다시 그 데이터를 inputWord()에 전달하여 
+		 * 데이터만(섞인 영단어) 되돌려 받고
+		 * 다시 그 데이터를 inputWord()에 전달하여
 		 * 수행하는 코드로 변경
 		 * 
 		 * 첫번째 방법은 inputWord()와 getSuffleWord()가
@@ -82,82 +86,88 @@ public class WordServiceImplV1 {
 		 * 한 method의 코드를 변경하여도
 		 * 다른 method코드는 변경하지 않아도 되는 구조가 되었다
 		 * 
-		 * 첫번째 코드는 결합도가 높은 코드
-		 * 두번째 코드는 결합도는 낮추고 응집도를 높인 코드가 된다
+		 * 첫번째코드는 결합도가 높은 코드
+		 * 두번째코드는 결합도는 낮추고 응집도를 높인 코드가 된다
 		 * 
-		 * SW공학에서 좋은 코드라고 하는 예 이다.
+		 * SW공학에서 좋은 코드라고 하는 예 이다
 		 */
 		String viewWord[] = this.getSuffleWord();
-		this.inputWord(viewWord);
+		this.inputWord( viewWord );
 	}
 	
-	
-	//startGame을 getSuffleWord로 변경
-	//1.wordList에서 단어를 한개 추출하고
-	//2.suffleEnglish()에게 단어를 전달하고
-	//	 suffle된 알파벳 배열을 return 받아 
-	//	 그대로 return 하기 
+	// startGame을 getSuffleWord로 변경
+	// 1. wordList에서 단어를 한개 추출하고
+	// 2. suffleEnglish() 에게 단어를 전달하고
+	//		suffle 된 알파벳 배열을 return받아
+	//		그대로 return 하기
 	protected String[] getSuffleWord() {
 		
+		// Random 클래스를 사용한 코드가
+		// startGame(), suffleWord() method에 나타났다
+		// Random 클래스를 사용한 객체 선언을
+		// 필드(맴버, 클래스)영역으로 보내자
+		// 그리고 생성자에서 초기화를 하자
+		// Random rnd = new Random();
 		
+		// wordList의 개수(size)를 담을 변수를
+		//  두개의 methd에서 사용을 하고 있다.
+		//	이 변수 필드영역으로 보내자
+		// int nWordCount = wordList.size();
 		
-		//Random 클래스를 사용한 코드가 
-		//startGame(), suffleWord() method에 나타났다
-		//Random클래스를 사용한 객체 선언을
-		//필드 (멤버, 클래스)영역으로 보내자
-		//그리고 생성자에서 초기화 하자
-		//Random rnd = new Random();
-		
-		//wordList의 개수(size)를 담을 변수를
-		//두개의 method에서 사용을 하고 있다
-		//이 변수 필드영역으로 보내자
-		//int nWordCount = wordList.size();
-		//0~(WordList.size() - 1)범위의 임의 정수 만들기
+		// 0 ~ (WordList.size() - 1) 범위의 임의 정수 만들기
 		int nWordIndex = rnd.nextInt(nWordCount);
-		//생성된 index값으로 wrodList중에 단어 1개를 추출
-		//wordVO에 담기
+		
+		// 생성된 index값으로 wordList중에 단어 1개를 추출
+		// wordVO에 담기
 		WordVO wordVO = wordList.get(nWordIndex);
-		//영문단어를 스펠링 단위로 잘라서 배열로 생성
 
-		
-		//영문단어 알파벳을 뒤섞기 코드를
-		//method로 분리하고
-		//원래 영문단어를 매개변수로 전달하고
-		//뒤섞인 단어를 return받아 다시 배열에 저장
-		
-		
-		String[] suffleEnglish = this.suffleword(wordVO.getEnglish());
-		//System.out.println(Arrays.toString(suffleEnglish));
+		// 영문단어를 매개변수로 전달하고
+		// 알파벳 단위로 분리되고 
+		//		뒤섞인 단어를 return 받아
+		// sufffleEnglish에 저장
+		String[] suffleEnglish 
+			= this.suffleWord(wordVO.getEnglish());
+		// System.out.println(Arrays.toString(suffleEnglish));
 		return suffleEnglish;
 	}
 	
-	
-	private void inputWord(String[] viewWord) {
+	protected void inputWord(String[] viewWord) {
 		
 		System.out.println("=".repeat(50));
 		System.out.println("뤼팡의 영단어 게임 V1");
 		System.out.println("-".repeat(50));
 		
-		System.out.println("다음 단어를 바르게 배열하여 입력!!");
-		//suffle된 영단어를 보여주기 
-		//this.startGame();
-		System.out.println(Arrays.toString(viewWord));
-		System.out.println(">>");
-		String strInput = scan.nextLine();
+		System.out.println("다음 단어를 바르게 바열하여 입력!!");
 		
+		// suffle된 영단어를 보여주기
+		// this.startGame();
+		System.out.println(Arrays.toString(viewWord));
+		System.out.print(">> ");
+		String strInput = scan.nextLine();
 	}
 	
-	
+	/*
+	 * 영문단어를 매개변수로 받아서
+	 * 알파벳단위로 자르고
+	 * 뒤 섞어 배열로 만든 후 return
+	 */
+	protected String[] suffleWord(String strEnglish) {
 		
-	private String[] suffleword(String strEnglish) {
+		// 영문단어를 스펠링 단위로 잘라서 배열로 생성
+		String suffleEnglish[] 
+				= strEnglish.split("");
 		
-		String suffleEnglish[] = strEnglish.split("");
 		int nCount = suffleEnglish.length;
-		for(int i = 0; i<1000; i++) {
-			//임의의 index값 2개를 만들고 
+		
+		// 몇번 섞을래?(1000번 섞기)
+		// 	( suffle )
+		for(int i = 0 ; i < 1000 ; i++) {
+			// 임의의 index값 2개를 만들고
 			int index1 = rnd.nextInt(nCount);
 			int index2 = rnd.nextInt(nCount);
-			//index1에 저장된 값과 , index2w에 저장된 값을 바꾸기
+
+			// index1에 저장된 값과, index2에 저장된 값을
+			// 바꾸기
 			String temp = suffleEnglish[index1];
 			suffleEnglish[index1] = suffleEnglish[index2];
 			suffleEnglish[index2] = temp;
@@ -165,9 +175,8 @@ public class WordServiceImplV1 {
 		return suffleEnglish;
 	}
 	
-	
-	private void loadWords(String wordFile) {
-		//TODO word.txt파일을 읽어 wordList만들어 두기 
+	protected void loadWords(String wordFile) {
+		// TODO word.txt 파일을 읽어 wordList 만들어 두기
 		FileReader fileReader = null;
 		BufferedReader buffer = null;
 		
@@ -175,24 +184,23 @@ public class WordServiceImplV1 {
 			fileReader = new FileReader(wordFile);
 			buffer = new BufferedReader(fileReader);
 			while(true) {
+				
 				String reader = buffer.readLine();
-				if(reader == null)break;
-				
+				if(reader == null) break;
 				String words[] = reader.split(":");
-				
 				WordVO wordVO = new WordVO();
 				wordVO.setEnglish(words[영어]);
 				wordVO.setKorea(words[한글]);
 				wordList.add(wordVO);
-
-	 		}
+				
+			}
 			
-			//여기에서 wordList size 를 계산하여
-			//nWordCount 변수에 담을수도 있지만
-			//복잡한 코드 속에 있는 관계로
-			//코드가 셋팅되는것을 보기가 다소 불편하여
-			//생성자 부분으로 이동하였다.
-			//nWordCount = wordList.size();
+			// 여기에서 wordList size를 계산하여
+			// nWordCount 변수에 담을수도 있지만
+			// 복잡한 코드 속에 있는 관계로
+			// 코드가 세팅되는 것을 보기가 다소 불편하여
+			// 생성자 부분으로 이동하였다.
+			// nWordCount = wordList.size();
 			
 			buffer.close();
 		} catch (FileNotFoundException e) {
@@ -202,11 +210,9 @@ public class WordServiceImplV1 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-	
-		
 	}
+	
+	
 	
 
 }
